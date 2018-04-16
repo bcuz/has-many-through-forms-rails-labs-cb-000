45ef5e2dfca44ffe6c583a60251719d7c0d39e8c
adam n <adam.nawroc@gmail.com>
2018-04-16 17:43:27 +0000

    might work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
 
   def create
+    # how do i pass the post_id here??
+    # find post by user id?
+
+    # comment class is supposed to do the linking, thats what makes a diff way not likely
+    # is there a way to tell what post is a user just via the comment and the user id? dont think so.
     comment = Comment.create(comment_params)
+    # binding.pry
     redirect_to comment.post
   end
 
